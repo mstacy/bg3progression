b68170d21f1889ededc17d3ee31d868039e2f2c7
Matt Stacy <matt.stacy@katalys.com>
2025-01-14 13:01:09 -0800

    localstorage saving

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useLayoutEffect, useState } from "react";
 // import Image from "next/image";
 import Accordion from "@mui/material/Accordion";
 // import AccordionActions from "@mui/material/AccordionActions";
@@ -9,95 +12,165 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import progression from "../../progression.json";
 
 type region = {
-  name: string;
-  link: string;
-  locations: location[];
+    name: string;
+    link: string;
+    locations: location[];
 };
 
 type location = {
-  name: string;
-  link: string;
-  quests: quest[];
-  items: item[];
+    name: string;
+    link: string;
+    quests: quest[];
+    items: item[];
 };
 
 type quest = {
-  name: string;
-  link: string;
+    name: string;
+    link: string;
 };
 
 type item = {
-  name: string;
-  link: string;
+    name: string;
+    link: string;
 };
 
 export default function Home() {
-  const buildRegion = (region: region) => {
-    return (
-      <Accordion key={region.name}>
-        <AccordionSummary>
-          <h2>{region.name}</h2>
-        </AccordionSummary>
-
-        <AccordionDetails>
-          {region.locations.map((location) => buildLocation(location))}
-        </AccordionDetails>
-      </Accordion>
+    const checkedBoxesKey = "checkedBoxes";
+    const [checkedBoxes, setCheckedBoxes] = useState<Record<string, boolean>>(
+        {}
     );
-  };
 
-  const buildLocation = (location: location) => {
-    return (
-      <Accordion key={location.name}>
-        <AccordionSummary>
-          <h3>{location.name}</h3>
-        </AccordionSummary>
+    useLayoutEffect(() => {
+        const saved =
+            typeof window !== "undefined"
+                ? localStorage.getItem(checkedBoxesKey)
+                : "{}";
+        const initial = JSON.parse(saved || "{}");
+        if (Object.keys(initial).length) setCheckedBoxes(initial);
+    }, []);
 
-        <AccordionDetails>
-          {!!location.quests.length && (
-            <div>
-              <h4>Quests</h4>
-              {buildQuests(location.quests)}
-            </div>
-          )}
+    useLayoutEffect(() => {
+        localStorage.setItem(checkedBoxesKey, JSON.stringify(checkedBoxes));
+    }, [checkedBoxes]);
 
-          {!!location.items.length && (
-            <div>
-              <h4>Items</h4>
-              {buildItems(location.items)}
-            </div>
-          )}
-        </AccordionDetails>
-      </Accordion>
-    );
-  };
+    const buildRegion = (region: region) => {
+        return (
+            <Accordion key={region.name} disableGutters>
+                <AccordionSummary>
+                    <div className="flex justify-between w-full">
+                        <h2>{region.name}</h2>
+                        <div>0%</div>
+                    </div>
+                </AccordionSummary>
 
-  const buildQuests = (quests: quest[]) => {
-    return (
-      <>
-        {quests.map((quest: quest) => (
-          <FormControlLabel
-            label={quest.name}
-            control={<Checkbox />}
-            key={quest.name}
-          />
-        ))}
-      </>
-    );
-  };
+                <AccordionDetails>
+                    {region.locations.map((location) =>
+                        buildLocation(location)
+                    )}
+                </AccordionDetails>
+            </Accordion>
+        );
+    };
 
-  const buildItems = (items: item[]) => {
-    return (
-      <>
-        {items.map((item: item) => (
-          <FormControlLabel
-            label={item.name}
-            control={<Checkbox />}
-            key={item.name}
-          />
-        ))}
-      </>
-    );
-  };
-  return <div>{buildRegion(progression.act1[0])}</div>;
+    const buildLocation = (location: location) => {
+        return (
+            <Accordion key={location.name} disableGutters>
+                <AccordionSummary>
+                    <h3>{location.name}</h3>
+                </AccordionSummary>
+
+                <AccordionDetails
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "20px",
+                    }}
+                >
+                    {!!location.quests.length && (
+                        <div className="flex flex-col">
+                            <h4>Quests</h4>
+                            {buildQuests(location.quests)}
+                        </div>
+                    )}
+
+                    {!!location.items.length && (
+                        <div className="flex flex-col">
+                            <h4>Items</h4>
+                            {buildItems(location.items)}
+                        </div>
+                    )}
+                </AccordionDetails>
+            </Accordion>
+        );
+    };
+
+    const buildQuests = (quests: quest[]) => {
+        return (
+            <>
+                {quests.map((quest: quest) => {
+                    if (!checkedBoxes[quest.name]) {
+                        checkedBoxes[quest.name] = false;
+                    }
+                    return (
+                        <FormControlLabel
+                            label={quest.name}
+                            control={<Checkbox />}
+                            key={quest.name}
+                            onChange={(e, checked) => {
+                                handleChange({
+                                    checked: checked,
+                                    name: quest.name,
+                                });
+                            }}
+                            checked={checkedBoxes[quest.name]}
+                        />
+                    );
+                })}
+            </>
+        );
+    };
+
+    const buildItems = (items: item[]) => {
+        return (
+            <>
+                {items.map((item: item) => {
+                    if (!checkedBoxes[item.name]) {
+                        checkedBoxes[item.name] = false;
+                    }
+                    return (
+                        <FormControlLabel
+                            label={item.name}
+                            control={<Checkbox />}
+                            key={item.name}
+                            onChange={(e, checked) => {
+                                handleChange({
+                                    checked: checked,
+                                    name: item.name,
+                                });
+                            }}
+                            checked={checkedBoxes[item.name]}
+                        />
+                    );
+                })}
+            </>
+        );
+    };
+
+    const handleChange = ({
+        checked,
+        name,
+    }: {
+        checked: boolean;
+        name: string;
+    }) => {
+        // console.log(e);
+
+        checkedBoxes[name] = checked;
+        setCheckedBoxes({
+            ...checkedBoxes,
+        });
+        console.log(checkedBoxes);
+    };
+
+    return <div>{buildRegion(progression.act1[0])}</div>;
 }
