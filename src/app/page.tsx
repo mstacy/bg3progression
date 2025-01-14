f46df408f73b015c7ded90fd4d67905f839308b7
Matt Stacy <matt.stacy@katalys.com>
2025-01-14 11:09:26 -0800

    accordions and checkboxes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,103 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+import Accordion from "@mui/material/Accordion";
+// import AccordionActions from "@mui/material/AccordionActions";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+// import styles from "./page.module.css";
+import progression from "../../progression.json";
+
+type region = {
+  name: string;
+  link: string;
+  locations: location[];
+};
+
+type location = {
+  name: string;
+  link: string;
+  quests: quest[];
+  items: item[];
+};
+
+type quest = {
+  name: string;
+  link: string;
+};
+
+type item = {
+  name: string;
+  link: string;
+};
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const buildRegion = (region: region) => {
+    return (
+      <Accordion key={region.name}>
+        <AccordionSummary>
+          <h2>{region.name}</h2>
+        </AccordionSummary>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <AccordionDetails>
+          {region.locations.map((location) => buildLocation(location))}
+        </AccordionDetails>
+      </Accordion>
+    );
+  };
+
+  const buildLocation = (location: location) => {
+    return (
+      <Accordion key={location.name}>
+        <AccordionSummary>
+          <h3>{location.name}</h3>
+        </AccordionSummary>
+
+        <AccordionDetails>
+          {!!location.quests.length && (
+            <div>
+              <h4>Quests</h4>
+              {buildQuests(location.quests)}
+            </div>
+          )}
+
+          {!!location.items.length && (
+            <div>
+              <h4>Items</h4>
+              {buildItems(location.items)}
+            </div>
+          )}
+        </AccordionDetails>
+      </Accordion>
+    );
+  };
+
+  const buildQuests = (quests: quest[]) => {
+    return (
+      <>
+        {quests.map((quest: quest) => (
+          <FormControlLabel
+            label={quest.name}
+            control={<Checkbox />}
+            key={quest.name}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+        ))}
+      </>
+    );
+  };
+
+  const buildItems = (items: item[]) => {
+    return (
+      <>
+        {items.map((item: item) => (
+          <FormControlLabel
+            label={item.name}
+            control={<Checkbox />}
+            key={item.name}
           />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        ))}
+      </>
+    );
+  };
+  return <div>{buildRegion(progression.act1[0])}</div>;
 }
