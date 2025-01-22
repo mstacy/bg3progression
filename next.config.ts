e95d864e37662c5bb60a85a4f20d39cb19152395
Matt Stacy <matt.t.stacy@gmail.com>
2025-01-22 11:08:01 -0800

    publish to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/bg3progression",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
